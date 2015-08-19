46545b3d721c3501468ad4c4669a5126af5237dc
wanghuan <hunter.v.wang@gmail.com>
2015-08-20 01:37:30 +0800

    add choujiang

--- t.php
+++ t.php
@@ -117,9 +117,9 @@
         form .submit{
             position: absolute;
             top: 250px;
-            left: 150px;
-            width: 604px;
-            height: 81px;
+            left: 130px;
+            width: 421px;
+            height: 84px;
             background: url("images/submit_btn2.png") no-repeat; text-align: center;border: 0;border-radius: 4px;
         }
     </style>
