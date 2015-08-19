375e9553263377b18cc281c693172aac2de14dcb
wanghuan <hunter.v.wang@gmail.com>
2015-08-19 14:23:55 +0800

    add choujiang

--- choujiang_1.php
+++ choujiang_1.php
@@ -41,15 +41,20 @@
 </head>
 <body>
 
-<div class="start main-stage">
-    </div>
+<img class="start main-stage" alt="Planets" usemap ="#planetmap" />
+<map name="planetmap">
+    <area shape="rect" coords="0,0,110,260" href="www.baidu.com" alt="Sun" />
+    <area shape="circle" coords="129,161,10" href="www.36kr.com" alt="Mercury" />
+    <area shape="circle" coords="180,139,14" href="www.qq.com" alt="Venus" />
+</map>
+    </img>
 <script type="application/javascript">
 
-    $(".start").on('click touchend', function(e){
-        e.preventDefault();
-        var myDate = new Date();
-        location.href="choujiang_2.php?from=index&timestamp=" + myDate.getTime();
-    });
+//    $(".start").on('click touchend', function(e){
+//        e.preventDefault();
+//        var myDate = new Date();
+//        location.href="choujiang_2.php?from=index&timestamp=" + myDate.getTime();
+//    });
 
 </script>
 </body>
