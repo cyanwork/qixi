62fb083762cc8134849b8aded90427dd3a8ceedd
wanghuan <hunter.v.wang@gmail.com>
2015-08-20 02:24:58 +0800

    add choujiang

--- server/qixi.php
+++ server/qixi.php
@@ -6,4 +6,39 @@
  * Time: 10:27
  */
 
-echo "OK".$_GET['phone'];
+$phone = $_GET['phone'];
+
+if(isset($phone)){
+    $con = mysql_connect("rdsp1e2zvi5og2cqgbqmw.mysql.rds.aliyuncs.com","rds_root","12345678");
+//echo "<br>1";
+    if (!$con)
+    {
+//        echo "<br>2";
+        die('Could not connect: ' . mysql_error());
+    }
+//echo "<br>3";
+
+    mysql_select_db("qixi", $con);
+    mysql_query("set names utf8;");
+//echo "<br>4";
+
+    $result = mysql_query("select * from zhongjiang;");
+    $i = 0;
+    while($row = mysql_fetch_array($result))
+    {
+        $i++;
+    }
+    if($i < 6){
+        $rand = rand(1,1000);
+        echo "rand:".$rand;
+        if($rand == 9){
+            mysql_query("insert into zhongjiang (zhong) values ('$phone')");
+            echo "中奖";
+        }
+
+    }
+    echo "wei zhongjiang";
+//echo "<br>5";
+    mysql_close($con);
+
+}
