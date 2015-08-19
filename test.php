21d3e8fc23f47da69cb76fed306d160910ae6265
wanghuan <hunter.v.wang@gmail.com>
2015-08-19 15:15:37 +0800

    add choujiang

--- test.php
+++ test.php
@@ -4,46 +4,62 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta content="就是这样吗？">
     <script type="application/javascript">
-        var imgUrl = 'https://www.baidu.com/img/bdlogo.png';  // 分享后展示的一张图片
-        var lineLink = 'https://www.baidu.com/img/bdlogo.png'; // 点击分享后跳转的页面地址
-        var descContent = "miaoshu";  // 分享后的描述信息
-        var shareTitle = 'titlesss';  // 分享后的标题
-        var appid = 'gh_ac13cb3b32b7';  // 应用id,如果有可以填，没有就留空
+        <script>
+        var imgUrl = 'http://topic.xcar.com.cn/201403/ad_q3/pic/banner.jpg';
+        var lineLink = 'http://topic.xcar.com.cn/201403/ad_q3/index.php';
+        var descContent = "http://topic.xcar.com.cn/201403/ad_q3/index.php";
+        var shareTitle = '【奥迪Q3开启尊享礼遇季】报名试驾，赢取精美礼品';
+        var appid = 'wxc9937e3a66af6dc8';
 
         function shareFriend() {
             WeixinJSBridge.invoke('sendAppMessage',{
                 "appid": appid,
                 "img_url": imgUrl,
-                "img_width": "200",
-                "img_height": "200",
+                "img_width": "640",
+                "img_height": "640",
                 "link": lineLink,
                 "desc": descContent,
                 "title": shareTitle
             }, function(res) {
-                //_report('send_msg', res.err_msg);  // 这是回调函数，必须注释掉
+                _report('send_msg', res.err_msg);
             })
         }
         function shareTimeline() {
             WeixinJSBridge.invoke('shareTimeline',{
                 "img_url": imgUrl,
-                "img_width": "200",
-                "img_height": "200",
+                "img_width": "640",
+                "img_height": "640",
                 "link": lineLink,
                 "desc": descContent,
                 "title": shareTitle
             }, function(res) {
-                //_report('timeline', res.err_msg); // 这是回调函数，必须注释掉
+                _report('timeline', res.err_msg);
+            });
+        }
+        function shareWeibo() {
+            WeixinJSBridge.invoke('shareWeibo',{
+                "content": descContent,
+                "url": lineLink,
+            }, function(res) {
+                _report('weibo', res.err_msg);
             });
         }
         // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
         document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+
             // 发送给好友
             WeixinJSBridge.on('menu:share:appmessage', function(argv){
                 shareFriend();
             });
+
             // 分享到朋友圈
             WeixinJSBridge.on('menu:share:timeline', function(argv){
                 shareTimeline();
+            });
+
+            // 分享到微博
+            WeixinJSBridge.on('menu:share:weibo', function(argv){
+                shareWeibo();
             });
         }, false);
     </script>
