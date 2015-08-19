3fce0d2a8e4afa323a28746cdd848ccadefc206b
wanghuan <hunter.v.wang@gmail.com>
2015-08-19 14:11:09 +0800

    add choujiang

--- choujiang_1.php
+++ choujiang_1.php
@@ -26,9 +26,7 @@
         .bg {
             width: 100%;
             height: 100%;
-            right: 0;
-            top: 0;
-            background: url("images/bg_cj.jpg")  no-repeat center center #333333;
+            background: url("images/bg_cj.jpg")  no-repeat;
         }
 
     </style>
