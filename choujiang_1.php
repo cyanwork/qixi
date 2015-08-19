21dc98235c58cb9c46eac8486666b374137de7b8
wanghuan <hunter.v.wang@gmail.com>
2015-08-19 14:13:09 +0800

    add choujiang

--- choujiang_1.php
+++ choujiang_1.php
@@ -21,19 +21,27 @@
             height: 100%;
             margin: 0;
             padding: 0;
+            overflow: hidden;
             background: #000;
         }
-        .bg {
-            width: 100%;
-            height: 100%;
-            background: url("images/bg_cj.jpg")  no-repeat;
+        .main-stage {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: url("images/bg_cj.jpg") no-repeat center center #333333;
+            background-size: cover;
+            opacity: 1;
+            -webkit-transition: all 0.4s;
+            transition: all 0.4s;
         }
 
     </style>
 </head>
 <body>
 
-<div class="start bg">
+<div class="start main-stage">
     </div>
 <script type="application/javascript">
 
