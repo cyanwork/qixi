1d82a3bb567878bca1120ec28969ce05df61635b
wanghuan <hunter.v.wang@gmail.com>
2015-08-19 20:53:24 +0800

    add choujiang

--- test.php
+++ test.php
@@ -3,13 +3,30 @@
     <title>七夕抽奖活动</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta content="就是这样吗？">
-    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-    <script type="application/javascript">
 
-    </script>
 </head>
 <body>
-<img src="images/bg_cj.jpg">
-<!--gh_ac13cb3b32b7   wx603d77659b810160 -->
+
+<?php
+
+$con = mysql_connect("rdsp1e2zvi5og2cqgbqmw.mysql.rds.aliyuncs.com","rds_root","12345678");
+echo "<br>1";
+if (!$con)
+{
+    echo "<br>2";
+    die('Could not connect: ' . mysql_error());
+}
+echo "<br>3";
+
+mysql_select_db("qixi", $con);
+echo "<br>4";
+mysql_query("INSERT INTO users ('city', 'name', 'phone')
+    VALUES ('12', '11', '32')");
+echo "<br>5";
+mysql_close($con);
+echo "<br>6";
+
+?>
+
 </body>
 </html>
