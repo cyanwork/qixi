dc89201d14774c636b291cd7c793a2cb4b3cbcf4
wanghuan <hunter.v.wang@gmail.com>
2015-08-20 09:42:15 +0800

    add choujiang

--- share.php
+++ share.php
@@ -1,70 +1,46 @@
 <html>
 <head>
 
-    <script type="application/javascript">
-        var imgUrl = "http://ww3.sinaimg.cn/bmiddle/6106a4f0gw1ev8tq2j1k9j20sg0dpte8.jpg";
-
-        var lineLink = "www.weibo.com";
-
-        var descContent = '描述语';
-
-        var shareTitle = '标题';
-
-        var appid = 'wxfa2416fbe6b39358';
-
-        function shareFriend() {
-
-            WeixinJSBridge.invoke('sendAppMessage',{
-
-                "appid": appid,
-
-                "img_url": imgUrl,
-
-                "img_width": "200",
-
-                "img_height": "200",
-
-                "link": lineLink,
-
-                "desc": descContent,
-
-                "title": shareTitle
-
-            }, function(res) {
-
-                //_report('send_msg', res.err_msg);
-
-            })
-
-        }
-
+    <script>
         document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-
+            window.shareData = {
+                "timeLineLink": "http://www.baidu.com",
+                "sendFriendLink": "http://www.baidu.com",
+                "weiboLink": "http://www.baidu.com",
+                "tTitle": "八斗美妆人才网",
+                "tContent": "共创会—八斗携手阿里O2O，聚合品牌、连锁渠道方共谋美妆未来商业模式",
+                "fTitle": "八斗美妆人才网",
+                "fContent": "共创会—八斗携手阿里O2O，聚合品牌、连锁渠道方共谋美妆未来商业模式",
+                "wContent": "共创会—八斗携手阿里O2O，聚合品牌、连锁渠道方共谋美妆未来商业模式"
+            };
             // 发送给好友
-
-            WeixinJSBridge.on('menu:share:appmessage', function(argv){
-
-                shareFriend();
-
+            WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+                WeixinJSBridge.invoke('sendAppMessage', {
+                    "img_url": "http://su.bdimg.com/static/superplus/img/logo_white.png",
+                    "img_width": "401",
+                    "img_height": "275",
+                    "link": window.shareData.sendFriendLink,
+                    "desc": window.shareData.fContent,
+                    "title": window.shareData.fTitle
+                }, function (res) {
+                    _report('send_msg', res.err_msg);
+                })
             });
-
             // 分享到朋友圈
-
-            WeixinJSBridge.on('menu:share:timeline', function(argv){
-
-//                shareTimeline();
-
+            WeixinJSBridge.on('menu:share:timeline', function (argv) {
+                WeixinJSBridge.invoke('shareTimeline', {
+                    "img_url": "http://su.bdimg.com/static/superplus/img/logo_white.png",
+                    "img_width": "401",
+                    "img_height": "275",
+                    "link": window.shareData.timeLineLink,
+                    "desc": window.shareData.tContent,
+                    "title": window.shareData.tTitle
+                }, function (res) {
+                    _report('timeline', res.err_msg);
+                });
             });
 
-            // 分享到微博
-
-            WeixinJSBridge.on('menu:share:weibo', function(argv){
-
-//                shareWeibo();
-
-            });
-
-        }, false);
+        }, false)
     </script>
 
 </head>
