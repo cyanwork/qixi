62b555b4feb2f39efa492728ca6167b7271a20e6
wanghuan <hunter.v.wang@gmail.com>
2015-08-20 10:25:46 +0800

    add choujiang

--- second_step3.php
+++ second_step3.php
@@ -1,12 +1,10 @@
-<?php
-//require_once('header.php');
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","errorBeacon":"bam.nr-data.net","licenseKey":"383240907d","applicationID":"4830918","transactionName":"dQlXRRFcDQ9SEE1DQFQKUEILVgU8UQ0QXkYZFVFeFA==","queueTime":1,"applicationTime":54,"agent":"js-agent.newrelic.com/nr-686.min.js"}</script>
+    <script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","errorBeacon":"bam.nr-data.net","licenseKey":"383240907d","applicationID":"4830918","transactionName":"dQlXRRFcDQ9SEE1DQFQKUEILVgU8UQ0QXkYZFVFeFA==","queueTime":1,"applicationTime":42,"agent":"js-agent.newrelic.com/nr-686.min.js"}</script>
     <script type="text/javascript">window.NREUM||(NREUM={}),__nr_require=function(e,n,t){function r(t){if(!n[t]){var o=n[t]={exports:{}};e[t][0].call(o.exports,function(n){var o=e[t][1][n];return r(o?o:n)},o,o.exports)}return n[t].exports}if("function"==typeof __nr_require)return __nr_require;for(var o=0;o<t.length;o++)r(t[o]);return r}({QJf3ax:[function(e,n){function t(e){function n(n,t,a){e&&e(n,t,a),a||(a={});for(var u=c(n),f=u.length,s=i(a,o,r),p=0;f>p;p++)u[p].apply(s,t);return s}function a(e,n){f[e]=c(e).concat(n)}function c(e){return f[e]||[]}function u(){return t(n)}var f={};return{on:a,emit:n,create:u,listeners:c,_events:f}}function r(){return{}}var o="nr@context",i=e("gos");n.exports=t()},{gos:"7eSDFh"}],ee:[function(e,n){n.exports=e("QJf3ax")},{}],3:[function(e,n){function t(e){return function(){r(e,[(new Date).getTime()].concat(i(arguments)))}}var r=e("handle"),o=e(1),i=e(2);"undefined"==typeof window.newrelic&&(newrelic=window.NREUM);var a=["setPageViewName","addPageAction","setCustomAttribute","finished","addToTrace","inlineHit","noticeError"];o(a,function(e,n){window.NREUM[n]=t("api-"+n)}),n.exports=window.NREUM},{1:12,2:13,handle:"D5DuLP"}],gos:[function(e,n){n.exports=e("7eSDFh")},{}],"7eSDFh":[function(e,n){function t(e,n,t){if(r.call(e,n))return e[n];var o=t();if(Object.defineProperty&&Object.keys)try{return Object.defineProperty(e,n,{value:o,writable:!0,enumerable:!1}),o}catch(i){}return e[n]=o,o}var r=Object.prototype.hasOwnProperty;n.exports=t},{}],D5DuLP:[function(e,n){function t(e,n,t){return r.listeners(e).length?r.emit(e,n,t):void(r.q&&(r.q[e]||(r.q[e]=[]),r.q[e].push(n)))}var r=e("ee").create();n.exports=t,t.ee=r,r.q={}},{ee:"QJf3ax"}],handle:[function(e,n){n.exports=e("D5DuLP")},{}],XL7HBI:[function(e,n){function t(e){var n=typeof e;return!e||"object"!==n&&"function"!==n?-1:e===window?0:i(e,o,function(){return r++})}var r=1,o="nr@id",i=e("gos");n.exports=t},{gos:"7eSDFh"}],id:[function(e,n){n.exports=e("XL7HBI")},{}],G9z0Bl:[function(e,n){function t(){var e=d.info=NREUM.info,n=f.getElementsByTagName("script")[0];if(e&&e.licenseKey&&e.applicationID&&n){c(p,function(n,t){n in e||(e[n]=t)});var t="https"===s.split(":")[0]||e.sslForHttp;d.proto=t?"https://":"http://",a("mark",["onload",i()]);var r=f.createElement("script");r.src=d.proto+e.agent,n.parentNode.insertBefore(r,n)}}function r(){"complete"===f.readyState&&o()}function o(){a("mark",["domContent",i()])}function i(){return(new Date).getTime()}var a=e("handle"),c=e(1),u=window,f=u.document;e(2);var s=(""+location).split("?")[0],p={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",agent:"js-agent.newrelic.com/nr-686.min.js"},d=n.exports={offset:i(),origin:s,features:{}};f.addEventListener?(f.addEventListener("DOMContentLoaded",o,!1),u.addEventListener("load",t,!1)):(f.attachEvent("onreadystatechange",r),u.attachEvent("onload",t)),a("mark",["firstbyte",i()])},{1:12,2:3,handle:"D5DuLP"}],loader:[function(e,n){n.exports=e("G9z0Bl")},{}],12:[function(e,n){function t(e,n){var t=[],o="",i=0;for(o in e)r.call(e,o)&&(t[i]=n(o,e[o]),i+=1);return t}var r=Object.prototype.hasOwnProperty;n.exports=t},{}],13:[function(e,n){function t(e,n,t){n||(n=0),"undefined"==typeof t&&(t=e?e.length:0);for(var r=-1,o=t-n||0,i=Array(0>o?0:o);++r<o;)i[r]=e[n+r];return i}n.exports=t},{}]},{},["G9z0Bl"]);</script>
-    <title>你的爱有多热？-雪佛兰七夕有礼</title>
+    <title>登记信息进行抽奖</title>
     <meta name="description" content="为登记信息进行抽奖提交数据。&lt;p&gt;&lt;span style=&quot;color: #ff0000;&quot;&gt;本次活动仅限 广东省、广西省、福建省、海南省 内的网友参加&lt;/span&gt;&lt;/p&gt;
 &lt;p&gt;&amp;nbsp;&lt;/p&gt;">
     <link rel="shortcut icon" type="image/x-icon" href="https://dn-jinshuju-assets.qbox.me/assets/favicon-fef7f8629e317cfe7151f53618d9b1f4.ico" />
@@ -72,6 +70,21 @@
         }
 
     </style>
+
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" media="screen" href="https://dn-jinshuju-assets.qbox.me/assets/ie/lte-ie8-6133413327d84df1b28bc4f708809c0d.css" />
+    <script src="https://dn-jinshuju-assets.qbox.me/assets/html5-4edc28a1bb1663a7d98f2a19c2df441a.js"></script>
+    <![endif]-->
+    <!--[if lte IE 7]>
+    <link rel="stylesheet" media="screen" href="https://dn-jinshuju-assets.qbox.me/assets/ie/lte-ie7-6e7a960483e7607f2c726be53ecfbf2f.css" />
+    <![endif]-->
+    <!--[if IE 6]> <link rel="stylesheet" media="screen" href="https://dn-jinshuju-assets.qbox.me/assets/ie/ie6-12765149c11a6a5ae600391183de1593.css" /> <![endif]-->
+    <script src="https://dn-jinshuju-assets.qbox.me/assets/published_forms/application-56c0e62deb6c6001b926b773c6775c5b.js"></script>
+    <!--[if IE 6]>
+    <script src="https://dn-jinshuju-assets.qbox.me/assets/published_forms/fix-ie6-36e0b9735c224cd555d5f27940d0fbba.js"></script>
+    <![endif]-->
+    <meta name="csrf-param" content="authenticity_token" />
+    <meta name="csrf-token" content="QO0cCLmQI9yNYiBA0Q3L7HBbfuh5JorOuAckBw4WoS5neZUWaEoc8M5k3GclhfH0cRVLfoifbFRNPtrhY8uwrg==" />
 </head>
 <body class="entry-container bg-image">
 
@@ -89,7 +102,7 @@
     </div>
 </header>
 
-<form class="center" data-form-token="KQUfFX" data-validate-url="/f/KQUfFX/validate_fields" id="new_entry" action="third_step.php?from=index&timestamp=2439995105204" accept-charset="UTF-8" method="post"  onsubmit="return form_sub()"><input type="hidden" name="utf8" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="qhM5GcW4bZ2v/6de/zZMhlgoXhaIeS5oEquSsmevvCSNh7AHFGJSsez5W3kLvnaeWWZrgHnAyPLnkmxUCnKtpA==" />
+<form class="center" data-form-token="KQUfFX" data-validate-url="/f/KQUfFX/validate_fields" id="new_entry" action="/f/KQUfFX" accept-charset="UTF-8" method="post"><input type="hidden" name="utf8" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="yGSnbpwJTzO6O6LJpmFxBqSHMHP5gUaCicWResLFh4Tv8C5wTdNwH/k9Xu5S6UsepckF5Qg4oBh8/G+crxiWBA==" />
     <div class="banner">
         <span class="banner-text banner-content"></span>
 
@@ -121,7 +134,7 @@
 
                             <div class="controls">
                                 <div class="dropdown ">
-                                    <select name="sf" id="entry_field_4" class="needsclick"><option value="">请选择</option><option selected="selected" value="Wmv5">广东省</option>
+                                    <select name="entry[field_4]" id="entry_field_4" class="needsclick"><option value="">请选择</option><option selected="selected" value="Wmv5">广东省</option>
                                         <option value="OQb9">广西省</option>
                                         <option value="Uk3P">福建省</option>
                                         <option value="GLz6">海南省</option></select>
@@ -145,7 +158,7 @@
                         <div class="field-content">
 
                             <div class="controls">
-                                <input type="text" value="" name="name" id="entry_field_12" />
+                                <input type="text" value="" name="entry[field_12]" id="entry_field_12" />
                             </div>
 
                         </div>
@@ -163,7 +176,7 @@
                         <div class="field-content">
 
                             <div class="controls">
-                                <input type="text" value="" name="phone" id="entry_field_13" />
+                                <input type="text" value="" name="entry[field_13]" id="entry_field_13" />
                             </div>
 
                         </div>
@@ -199,7 +212,7 @@
 
                     <input type="submit" name="commit" value="提交" data-disable-with="提交中..." class="submit btn btn-primary" />
                     <span class="page-number text-muted"></span>
-                    <a target="_blank" class="report-form" href="https://jinshuju.net/f/K7GcOZ?x_field_1=KQUfFX"></a>
+                    <a target="_blank" class="report-form" href="https://jinshuju.net/f/K7GcOZ?x_field_1=KQUfFX">举报</a>
                     <input type="hidden" name="embedded" id="embedded" />
                     <input type="hidden" name="banner" id="banner" />
                     <input type="hidden" name="background" id="background" />
@@ -240,35 +253,100 @@
             </div><div class="modal-footer"><a data-dismiss="modal" class="btn btn-info" href="javascript:void(0)">返回修改</a></div></div></div></div>
 
 
-<script src="scripts/main.js"></script>
-<!--<script type="application/javascript">-->
-<!---->
-<!--//    $(".submit").on('click touchend', function(e){-->
-<!--//        e.preventDefault();-->
-<!--//        alert("");-->
-<!--//    });-->
-<!---->
-<!--</script>-->
 
-<script type="application/javascript">
-    function form_sub()
-    {
-        var name = $(".form_input1").val();
-        var phone = $(".form_input2").val();
-        if(name == ""){
-            alert("姓名不能为空");
-            return false;
-        }
-        if(phone == ""){
-            alert("手机号不能为空");
-            return false;
-        }
-        return true;
+
+
+
+<footer class='published'>
+    <div class="center ">
+        <a target="_blank" class="powered-by" href="https://jinshuju.net/?utm_campaign=jul&amp;utm_medium=bottom_logo&amp;utm_source=published_form&amp;utm_term=KQUfFX">
+            <i class="gd-icon-logo powered-logo"></i>
+            <p>Powered By 金数据</p>
+        </a>    </div>
+</footer>
+
+<script>
+    //<![CDATA[
+
+    $(function(){
+        GoldenData = GoldenData || {};
+        GoldenData.getFormMessages('/f/KQUfFX/form_message');
+    });
+
+    $(function() {
+        $(document).on('mousedown', 'input.submit', function(){ga('send', 'event', 'PublicForm', 'Submit', "55d522cec9375f5c2a000210");});
+        $(document).on('mousedown', '.corner-qrcode', function(){ga('send', 'event', 'PublicForm', '点击表单二维码分享', "55d522cec9375f5c2a000210");});
+        $('.goods-warning').delay(1000).slideDown(500);
+    });
+
+
+    //]]>
+</script>
+<script>
+    //<![CDATA[
+
+    $(function() {
+        GoldenData = GoldenData || {};
+        GoldenData.attachmentIconPath = {
+            "default": "https://dn-jinshuju-assets.qbox.me/assets/attachment-a1e0d08b7d5ece3ddbcf0fddced93238.png",
+            pdf: "https://dn-jinshuju-assets.qbox.me/assets/pdf-1e06b28f6f2396e209f8f9b934200a0b.png",
+            txt: "https://dn-jinshuju-assets.qbox.me/assets/txt-a0c11c654f8adc0ca7039fb766d9dd50.png",
+            doc: "https://dn-jinshuju-assets.qbox.me/assets/doc-53f9b357e0f84c701815e3ec2ec67c51.png",
+            docx: "https://dn-jinshuju-assets.qbox.me/assets/docx-30b318fafbfc669e559466b5ac8b82f4.png",
+            ppt: "https://dn-jinshuju-assets.qbox.me/assets/ppt-9cadfaf07d8ebb89e16cbbeae3c7d1be.png",
+            pptx: "https://dn-jinshuju-assets.qbox.me/assets/pptx-9bd5f1139e11429ff3e7cb32e0085c72.png",
+            xls: "https://dn-jinshuju-assets.qbox.me/assets/xls-885540acc23467398f5c2bbfd5aebef7.png",
+            xlsx: "https://dn-jinshuju-assets.qbox.me/assets/xlsx-6621504c8c1a6fd0e899bf9d3544b17a.png",
+            jpg: "https://dn-jinshuju-assets.qbox.me/assets/jpg-bf1addf8093996535561ad9e8c2fdb9e.png",
+            png: "https://dn-jinshuju-assets.qbox.me/assets/png-f47018c26aa57ff127def12203f10ab2.png",
+            rar: "https://dn-jinshuju-assets.qbox.me/assets/rar-61c4ad0b08702c5a625f7c1a3a0d914b.png",
+            zip: "https://dn-jinshuju-assets.qbox.me/assets/zip-19dc8f111f8e678f6f3246fc11224c6a.png",
+            mp3: "https://dn-jinshuju-assets.qbox.me/assets/mp3-eb7a6bf41646398029db8abc3e177f70.png",
+            mp4: "https://dn-jinshuju-assets.qbox.me/assets/mp4-e1a99a78cd64d9fae552fdd69ccc973c.png",
+            avi: "https://dn-jinshuju-assets.qbox.me/assets/avi-b62704e72d73740cfbed0e86c66a1dfb.png",
+            gif: "https://dn-jinshuju-assets.qbox.me/assets/gif-6d085a9780877e9e6bfbedf9a404196d.png",
+            jpeg: "https://dn-jinshuju-assets.qbox.me/assets/jpeg-f5f134ea1959fb3643651ac7e156049d.png",
+            mpg: "https://dn-jinshuju-assets.qbox.me/assets/mpg-71bbeebb431f380b8d7d88582ba67ed6.png",
+            rm: "https://dn-jinshuju-assets.qbox.me/assets/rm-fd1c2bd5005bf8291dabf0ac366a8c3a.png",
+            mov: "https://dn-jinshuju-assets.qbox.me/assets/mov-cdfe55fe94084bcc483841e6470e9c09.png"
+        };
+
+
+        GoldenData.attachmentIcon = function(name) {
+            var baseName = "default";
+            if(typeof(name) != "undefined") {
+                baseName = name.split('.')[0];
+            }
+            baseName = baseName in GoldenData.attachmentIconPath ? baseName : 'default'
+            return GoldenData.attachmentIconPath[baseName];
+        };
+
+        GoldenData.zeroClipboardFlash = "https://dn-jinshuju-assets.qbox.me/assets/ZeroClipboard-68f5293f9b30d02c65d18305f0caf570.swf";
+        GoldenData.tinyMCEAsset = "https://dn-jinshuju-assets.qbox.me/assets/tinymce/content.css";
+        GoldenData.tinyMCEFieldInsertionAsset = "https://dn-jinshuju-assets.qbox.me/assets/tinymce/field-insertion-content.css";
+        GoldenData.mapMarkerDefault = "https://dn-jinshuju-assets.qbox.me/assets/map_marker_default-c51bd0cc9af7eba32cf4fb71f66d972f.png";
+        GoldenData.mapMarkerSelected = "https://dn-jinshuju-assets.qbox.me/assets/map_marker_selected-fd39874d1a02153f0477f8a1ab934dc9.png";
+    });
+
+    //]]>
+</script>
+<script>
+    if(document.querySelectorAll("script[src*='www.google-analytics.com']").length === 0) {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     }
 
+    ga('create', 'UA-48208031-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+
+    // for backup only
+    ga('create', 'UA-32460118-3', 'jinshuju.net', 'lagacyTracker');
+    ga('lagacyTracker.send', 'pageview');
 
 </script>
-
 
 </body>
 </html>
